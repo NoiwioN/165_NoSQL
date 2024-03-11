4b2dd0b278b7e7f075d9b976a616e3c3f462c2d5
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-03-11 14:42:38 +0100

    It works, user needs to be created

--- src/main/java/net/gibb/Main.java
+++ src/main/java/net/gibb/Main.java
@@ -1,16 +1,26 @@
 package net.gibb;
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.QueryConfig;
 
 public class Main {
     public static void main(String[] args)  {
     final String dbUri = "neo4j://localhost:7687";
-    final String dbUser = "neo4j";
-    final String dbPassword = "12345678";
+    final String dbUser = "test";
+    final String dbPassword = "sml12345";
 
         try (var driver = GraphDatabase.driver(dbUri, AuthTokens.basic(dbUser, dbPassword))) {
-            driver.verifyConnectivity();
+            var result = driver.executableQuery("MATCH (c:Climber{proficiency:\"Proficient\"}) return c as record").withConfig(QueryConfig.builder().withDatabase("neo4j").build())
+                    .execute();
+
+            var records = result.records();
+            records.forEach(r -> {
+                System.out.println(r);  // or r.get("name").asString(
+            });
+
         }
+
+
 
 
     }
